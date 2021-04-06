9420a00a7276423e0a41d10b6d87b780ffd0e538
SyedKomail <SyedKomail@users.noreply.github.com>
2021-04-06 07:27:48 -0700

    index.html update

--- script.js
+++ script.js
@@ -1,20 +1,81 @@
 $(function () {
   loadRecipies();
   $("#product").on("click", ".btn-danger", handleDelete);
-  //$("#addBtn").click(addRecipe);
-});
-function handleDelete() {
-  var btn = $(this);
+  $("#product").on("click", ".btn-warning", handleUpdate);
 
+  $("#addBtn").click(addRecipe);
+  $("#updateSave").click(function () {
+    var id = $("#updateId").val();
+    var title = $("#updateTitle").val();
+    var price = $("#updatePrice").val();
+    var color = $("#updateColor").val();
+    var department = $("#updateDepartment").val();
+    var description = $("#updateDescription").val();
+
+    $.ajax({
+      url: "https://usman-recipes.herokuapp.com/api/products/" + id,
+      data: { title, price, color, department, description },
+      method: "PUT",
+      success: function (response) {
+        console.log(response);
+        loadRecipies();
+        $("#updateModal").modal("hide");
+      },
+    });
+  });
+});
+
+function handleUpdate() {
+  var btn = $(this);
   var parentDiv = btn.closest(".product");
   let id = parentDiv.attr("data-id");
-  console.log("dsda");
+  $.get(
+    "https://usman-recipes.herokuapp.com/api/products/" + id,
+    function (response) {
+      $("#updateId").val(response._id);
+      $("#updateTitle").val(response.name);
+      $("#updatePrice").val(response.price);
+      $("#updateColor").val(response.color);
+      $("#updateDepartment").val(response.department);
+      $("#updateDescription").val(response.description);
+      $("#updateModal").modal("show");
+    }
+  );
+}
+
+function addRecipe() {
+  var name = $("#title").val();
+  var price = $("#price").val();
+  var color = $("#color").val();
+  var description = $("#description").val();
+  var department = $("#department").val();
+
+  $.ajax({
+    url: "https://usman-recipes.herokuapp.com/api/products/",
+    method: "POST",
+    data: { name, price, color, department, description },
+    success: function (response) {
+      console.log(response);
+      $("#name").val("");
+      $("#price").val("");
+      $("#description").val("");
+      $("#department").val("");
+      loadRecipies();
+      $("#addModal").modal("hide");
+    },
+  });
+}
+function handleDelete() {
+  console.log("del");
+  var btn = $(this);
+  var parentDiv = btn.closest(".product");
+  let id = parentDiv.attr("data-id");
+  console.log(id);
   console.log(id);
   $.ajax({
-    url: "https://usman-recipes.herokuapp.com/api/products" + id,
+    url: "https://usman-recipes.herokuapp.com/api/products/" + id,
     method: "DELETE",
     success: function () {
-      console.log("dsdad");
       loadRecipies();
     },
   });
@@ -31,7 +92,7 @@ function loadRecipies() {
       for (var i = 0; i < response.length; i++) {
         var rec = response[i];
         recipes.append(
-          `<div class="recipe" data-id="${rec._id}"></h3><p><h3> Name: ${rec.name}   </h3><h2> Price: ${rec.price}  </h2> Department: ${rec.department}</h3> </br> Description: ${rec.description}</h3><p><button class="btn btn-danger btn-sm float-right">delete</button> </p> </br> <p><button class="btn btn-warning btn-sm float-right">edit</button> </p></div>`
+          `<div class="product" data-id="${rec._id}"></h3><p><h3> Name: ${rec.name}   </h3><h2> Price: ${rec.price} </h2>   Color: ${rec.color}</h3>  </h2> </br> Department: ${rec.department}</h3> </br> Description: ${rec.description}</h3><p><button class="btn btn-danger btn-sm float-right">delete</button> </p> </br> <p><button class="btn btn-warning btn-sm float-right">edit</button> </p></div>`
         );
         // recipes.append("<div><h3>" + rec.title + "</h3></div>");
       }
